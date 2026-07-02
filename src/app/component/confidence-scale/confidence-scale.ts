b7fae65259d026a05e0c8e2114b4986f032cd672
IveNooName <fsc159548@stud.gibb.ch>
2026-07-02 07:55:42 +0200

    added CSS

--- src/app/component/confidence-scale/confidence-scale.ts
+++ src/app/component/confidence-scale/confidence-scale.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { MatProgressBar } from '@angular/material/progress-bar';
-import { MatCardContent } from '@angular/material/card';
+import { MatCard, MatCardContent } from '@angular/material/card';
 
 @Component({
   selector: 'app-confidence-scale',
-  imports: [MatProgressBar, MatCardContent],
+  imports: [MatProgressBar, MatCardContent, MatCard],
   templateUrl: './confidence-scale.html',
   styleUrl: './confidence-scale.scss',
 })
