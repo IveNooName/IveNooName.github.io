a5cafc0ff2ca56a1a719f014ea1b39dcb0e433aa
IveNooName <fsc159548@stud.gibb.ch>
2026-07-05 13:30:48 +0200

    added product cards

---
+++ src/app/components/projects-page/project-card/project-card.ts
@@ -0,0 +1,10 @@
+import { Component, inject, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-project-card',
+  imports: [],
+  templateUrl: './project-card.html',
+  styleUrl: './project-card.scss',
+})
+export class ProjectCard {
+}
