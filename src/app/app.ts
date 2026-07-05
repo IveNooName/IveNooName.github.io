d3172ab6546a4cf09b71e6609ef504613e80c476
IveNooName <fsc159548@stud.gibb.ch>
2026-07-05 15:26:31 +0200

    added landing page back

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ProjectsPage } from './components/projects-page/projects-page';
+import { LandingPage } from './components/landing-page/landing-page';
 
 @Component({
   selector: 'app-root',
-  imports: [ProjectsPage],
+  imports: [ProjectsPage, LandingPage],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
