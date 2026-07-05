ea6e8edef158e9be99d6b55e8f1d8b277d1d6e23
IveNooName <fsc159548@stud.gibb.ch>
2026-07-05 14:17:07 +0200

    made basic material card

--- src/app/components/projects-page/project-card/project-card.ts
+++ src/app/components/projects-page/project-card/project-card.ts
@@ -1,13 +1,28 @@
 import { Component, inject, input, signal } from '@angular/core';
 import { Project } from '../../../service/project';
 import { NgOptimizedImage } from '@angular/common';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardImage, MatCardTitle } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-project-card',
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardImage,
+    MatCardTitle,
+    MatCardActions,
+    MatButton,
+  ],
   templateUrl: './project-card.html',
   styleUrl: './project-card.scss',
 })
 export class ProjectCard {
   readonly project = input.required<Project>();
+
+  projectLink(): void {
+    location.href=this.project().repoLink;
+  }
+
 }
