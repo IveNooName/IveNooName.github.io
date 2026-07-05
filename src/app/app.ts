39f57db75d3a8374e1e4d92b0c5bcfae09246250
IveNooName <fsc159548@stud.gibb.ch>
2026-07-05 11:59:16 +0200

    added Projects page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LandingPage } from './components/landing-page/landing-page';
+import { ProjectsPage } from './components/projects-page/projects-page';
 
 @Component({
   selector: 'app-root',
-  imports: [LandingPage],
+  imports: [LandingPage, ProjectsPage],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
