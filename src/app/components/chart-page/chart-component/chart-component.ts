ddd2cdc981f7433fdaa451ccc07a7d1b69f483cb
IveNooName <fsc159548@stud.gibb.ch>
2026-07-20 19:55:48 +0100

    added number for the side

--- src/app/components/chart-page/chart-component/chart-component.ts
+++ src/app/components/chart-page/chart-component/chart-component.ts
@@ -1,14 +1,18 @@
 import { Component, computed, input } from '@angular/core';
 import { Language } from '../../../service/language';
+import { MatDivider } from '@angular/material/list';
 
 @Component({
   selector: 'app-chart-component',
-  imports: [],
+  imports: [MatDivider],
   templateUrl: './chart-component.html',
   styleUrl: './chart-component.scss',
 })
 export class ChartComponent {
+  totalLines = 10;
+
   languages = input.required<Language[]>();
+  numbersForStat: number[] | undefined;
 
   chartData = computed(() => {
     const langs = this.languages();
@@ -24,4 +28,11 @@ export class ChartComponent {
     }));
   });
 
+  ngOnInit() {
+    this.numbersForStat = new Array<number>(this.totalLines + 1);
+    this.numbersForStat[0] = 0
+    for (let i = 1; i <= this.totalLines; i++) {
+      this.numbersForStat[i] = i * 10
+    }
+  }
 }
